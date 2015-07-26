b78571bf486248189cc964206889f25c60bbbde8
avdata99 <andres@data99.com.ar>
2015-07-26 14:57:10 -0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 # rsync -av --progress --rsh='ssh -p 987' --exclude='NICathon/data/' --exclude='blog' opendata@opendatacordoba.org:/home/opendata/www/ ./
 
 #push
-# rsync -av --progress --rsh='ssh -p 987' --exclude='NICathon/data/' --exclude='blog' ./ opendata@opendatacordoba.org:/home/opendata/www/
+# rsync -av --progress --rsh='ssh -p 987' --exclude='.git' ./ opendata@opendatacordoba.org:/home/opendata/www/
