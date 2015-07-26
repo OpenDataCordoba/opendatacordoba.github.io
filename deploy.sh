c21816b8c88ce3c8f91bc1141f6ce76a55aead18
avdata99 <andres@data99.com.ar>
2015-07-26 14:50:50 -0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+# pull
+# rsync -av --progress --rsh='ssh -p 987' --exclude='NICathon/data/' --exclude='blog' opendata@opendatacordoba.org:/home/opendata/www/ ./
+
+#push
+# rsync -av --progress --rsh='ssh -p 987' --exclude='NICathon/data/' --exclude='blog' ./ opendata@opendatacordoba.org:/home/opendata/www/
